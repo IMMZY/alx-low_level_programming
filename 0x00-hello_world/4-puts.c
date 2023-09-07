041f8e010cda26aeb154e28cd986a22323fdc990
Ime Essien <imeessienime@gmail.com>
2023-09-07 07:28:07 -0700

    Hello puts task-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (success)
+ */
+
+#include <stdio.h>
+
+int main(void){
+	char name [100]
+	puts("Programming is like building a multilingual puzzle\n");
+	puts(name)
+	return (0);
+}
