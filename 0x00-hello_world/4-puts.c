a96116095627db63036c6d19cb5fabca34649171
Ime Essien <imeessienime@gmail.com>
2023-09-07 07:37:47 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 /**
  *main - entry point
  *
@@ -8,6 +9,6 @@
 #include <stdio.h>
 
 int main(void){
-	puts("\Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
