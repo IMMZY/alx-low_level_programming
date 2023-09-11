c5888b54e9935b57f64d0a956b4b801074593dcb
Ime Essien <imeessienime@gmail.com>
2023-09-11 06:45:07 -0700

    Print comb 4 task-11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible different combinations
+ * of thress digits.
+ *
+ * Return: 0
+ */
+#include <stdio.h>
+/**
+ * main - program that prints all possible different combinations
+ * of thress digits.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			if (j > i)
+			{
+				for (k = 48; k < 58; k++)
+				{
+					if (k > j)
+					{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == 55 && j == 56 && k == 57)
+					{
+						break;
+					}
+
+					putchar(',');
+					putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
