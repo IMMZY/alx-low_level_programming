f87aebbb3342f5083ca4091ce5d1c2c828838c5c
Ime Essien <imeessienime@gmail.com>
2023-09-12 08:03:48 -0700

    Putchar task-0 updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * main - Function that prints _putchar
- * Return : returns 0;
+ * main - Print "_putchar"
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -15,8 +16,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-
 	_putchar('\n');
-
 	return (0);
 }
