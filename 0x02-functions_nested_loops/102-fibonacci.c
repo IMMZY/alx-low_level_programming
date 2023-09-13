6f0ad7a3abd89d81d982069b73a8dcd4fe208acb
Ime Essien <imeessienime@gmail.com>
2023-09-13 06:32:04 -0700

    Fibonnaci task-14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 numbers of the fibonacci sequence
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int num1 = 1;
+	unsigned long int num2 = 2;
+	unsigned long int next;
+	int i;
+
+	printf("%lu, ", num1);
+	for (i = 1; i < 50; i++)
+	{
+		printf("%lu", num2);
+		next = num1 + num2;
+		num1 = num2;
+		num2 = next;
+		if (i != 49)
+			printf(", ");
+	}
+	printf("\n")
+	return (0);
+}
