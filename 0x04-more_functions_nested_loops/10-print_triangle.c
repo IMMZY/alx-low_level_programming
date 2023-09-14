699ec0a0454cd848e13773949d3cae0a5ed3f3cd
Ime Essien <imeessienime@gmail.com>
2023-09-14 15:24:33 -0700

    10-Print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle of square accoring to parameter
+ * @size: numbers of lines.
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 1; b < (size - a); b++)
+			_putchar(' ');
+		for (b--; b < size; b++)
+			_putchar(35);
+		if (b < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
