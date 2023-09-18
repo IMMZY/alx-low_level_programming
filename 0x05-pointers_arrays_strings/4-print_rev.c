05f8605231c02cbfe25900694c25ad80d4c6df01
Ime Essien <imeessienime@gmail.com>
2023-09-18 12:39:11 -0700

    4-print Reverse task-4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse string
+ *
+ * @s: String to be reversed
+ *
+ * Return: void
+ */
+
+
+void print_rev(char *s)
+{
+	int a;
+	int count = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		count++;
+	}
+	for (a = count - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
