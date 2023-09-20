85b3c42c8376e61aa3561097b84ccab385c0d449
Ime Essien <imeessienime@gmail.com>
2023-09-20 09:03:10 -0700

    1-strncat Task-1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int _length = strlen(dest);
+	int a;
+
+	for (a = 0; a < n && *src != '\0'; a++)
+	{
+		dest[_length + a] = src[a];
+		src++;
+	}
+	dest[_length + a] = '\0';
+	return (dest);
+}
