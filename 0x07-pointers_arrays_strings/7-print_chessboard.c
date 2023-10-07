9c2301d4f05c9d73d3adecadf7ff3cb0681721ce
Ime Essien <imeessienime@gmail.com>
2023-10-07 11:33:15 -0700

    7-print_chessboard task-6 updated

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * print_chessboard - Prints chessboard
- *
- * @a: Double pointer
- *
- * Return: 0
- */
+ * print_chessboard - prints chessboard.
+ * @a: matrix
+*/
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned int m, n;
+	int i, j;
 
-	for (m = 0; m < 8; m++)
+	for (i = 0; i < 8; i++)
 	{
-		for (n = 0; n < 8; n++)
-			_putchar(a[m][n]);
-		_putchar('\n');
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(*(*(i + a) + j));
+		}
+	_putchar('\n');
 	}
 }
