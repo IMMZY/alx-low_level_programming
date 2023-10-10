30364444d66f87b9325f36592ca85b38e006364d
Ime Essien <imeessienime@gmail.com>
2023-10-10 03:29:30 -0700

    dog task-0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG__H
+#define DOG__H
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
